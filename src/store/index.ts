8aca96ebc036108678725b0454e63c506cc1cf79
Burak <brkcrdk@gmail.com>
2019-11-24 21:17:35 +0200

    store actions

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,3 +2,16 @@ import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./rootReducer";
+
+const configureStore = () => {
+  const middleware = [thunkMiddleware];
+  const middlewareEnhancer = applyMiddleware(...middleware);
+
+  const store = createStore(
+    rootReducer,
+    composeWithDevTools(middlewareEnhancer)
+  );
+  return store;
+};
+
+export default configureStore;
