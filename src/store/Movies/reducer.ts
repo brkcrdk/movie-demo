ff6862bd230b49c749173a67b23f97d3879eb847
Burak <brkcrdk@gmail.com>
2019-11-25 16:13:32 +0200

    Movies action

--- src/store/Movies/reducer.ts
+++ src/store/Movies/reducer.ts
@@ -14,7 +14,15 @@ const initialState: MoviesState = {
 
 const moviesReducer = (state = initialState, action: MoviesActions) => {
   switch (action.type) {
+    case MOVIES_FETCH:
+      return { ...state, isLoading: true };
+    case MOVIES_SUCCESS:
+      return { ...state, isLoading: false, movies: action.payload };
+    case MOVIES_ERROR:
+      return { ...state, isLoading: false, error: action.error };
     default:
       return state;
   }
 };
+
+export default moviesReducer;
