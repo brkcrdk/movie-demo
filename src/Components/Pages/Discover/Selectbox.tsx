7ffb4b3bdaf48b11d759588b96f72d32a4090b69
Burak <brkcrdk@gmail.com>
2019-11-26 22:52:50 +0200

    Dropdown added for genres

--- src/Components/Pages/Discover/Selectbox.tsx
+++ src/Components/Pages/Discover/Selectbox.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useCallback, useState } from "react";
+import React, { useEffect, useCallback, useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchGenres } from "../../../store/Genres/action";
 import { addTag } from "../../../store/DiscoverFilter/action";
 import { Genre } from "../../../store/serverTypes";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 interface Props {}
 interface GenreProps {
   genreStore: {
@@ -11,11 +13,59 @@ interface GenreProps {
     };
   };
 }
+
+interface DropdownContent {
+  toggle: boolean;
+}
+const Dropdown = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+const DropdownHeader = styled.button`
+  background-color: transparent;
+  border: 0.3px solid grey;
+  padding: 0.5em 2em 0.5em 0.5em;
+`;
+const DropdownContent = styled.div`
+  display: ${(p: DropdownContent) => (p.toggle ? "block" : "none")};
+  position: absolute;
+  background-color: #f1f1f1;
+  max-height: 5em;
+  width: 100%;
+  z-index: 1;
+  transition: max-height 0.5s;
+`;
+const DropdownItem = styled(Link)`
+  &&& {
+  }
+`;
+
 const Selectbox: React.FC<Props> = () => {
+  const [toggle, setToggle] = useState(false);
+  const btnRef = useRef<HTMLButtonElement>(null);
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+  function useOutsideAlerter(ref: any) {
+    function handleClickOutside(event: any) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setToggle(false);
+      }
+    }
+    useEffect(() => {
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    });
+  }
+  useOutsideAlerter(btnRef);
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchGenres());
   }, [dispatch]);
+
   const genres = useSelector(
     (state: GenreProps) => state.genreStore.genre.genres
   );
@@ -29,9 +79,15 @@ const Selectbox: React.FC<Props> = () => {
   );
 
   return (
-    <div>
-      <select onChange={handleChange}>
-        <option disabled defaultValue="ssss">
+    <Dropdown>
+      <DropdownHeader ref={btnRef} onClick={handleToggle}>
+        Choose Genre
+      </DropdownHeader>
+      <DropdownContent toggle={toggle}>
+        <DropdownItem to="/">Burak</DropdownItem>
+      </DropdownContent>
+      {/* <select onChange={handleChange}>
+        <option disabled defaultValue="">
           Choose Genre
         </option>
         {genres !== undefined
@@ -41,8 +97,8 @@ const Selectbox: React.FC<Props> = () => {
               </option>
             ))
           : ""}
-      </select>
-    </div>
+      </select> */}
+    </Dropdown>
   );
 };
 
