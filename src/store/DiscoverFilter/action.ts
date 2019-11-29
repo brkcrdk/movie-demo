f3379968a84c5e3e2857a344e96e9e45fb1e86b5
Burak <brkcrdk@gmail.com>
2019-11-29 18:23:40 +0200

    Sort action

--- src/store/DiscoverFilter/action.ts
+++ src/store/DiscoverFilter/action.ts
@@ -1,4 +1,4 @@
-import { ADD_TAG, REMOVE_TAG } from "./types";
+import { ADD_TAG, REMOVE_TAG, SORT_BY } from "./types";
 
 export const addTag = (tags: { name: string; id: number }[]) => {
   return {
@@ -11,5 +11,12 @@ export const removeTag = (tags: {}[]) => {
   return {
     type: REMOVE_TAG,
     payload: tags
+  };
+};
+
+export const sortBy = (sort: string) => {
+  return {
+    type: SORT_BY,
+    payload: sort
   };
 };
