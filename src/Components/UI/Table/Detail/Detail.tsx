0f25b4492839b3c2c81d8e9c1d758524d63a82a3
Burak <brkcrdk@gmail.com>
2019-12-02 17:02:21 +0200

    Detail page slider added

--- src/Components/UI/Table/Detail/Detail.tsx
+++ src/Components/UI/Table/Detail/Detail.tsx
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
+import Slider from "react-slick";
 import { Expandable, Content } from "./DetailStyle";
+import { useDispatch, useSelector } from "react-redux";
 interface Props {
   activeIndex: number;
   index: number;
 }
 
 const Detail: React.FC<Props> = ({ activeIndex, index }) => {
+  const dispatch = useDispatch();
+  useEffect(() => {}, []);
   return (
     <Expandable>
       <Content expand={activeIndex === index}>
-        <div>Slayt</div>
+        <div>
+          <Slider></Slider>
+        </div>
         <div>Actors</div>
       </Content>
     </Expandable>
