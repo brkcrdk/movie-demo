51565312f2a70f0b70a3bff4af58c893c577f794
Burak <brkcrdk@gmail.com>
2019-12-02 10:57:14 +0200

    Bug fix for toggle actions

--- src/store/Toggles/reducer.ts
+++ src/store/Toggles/reducer.ts
@@ -13,8 +13,14 @@ const initialState: ToggleState = {
 const toggleReducer = (state = initialState, action: ToggleActions) => {
   switch (action.type) {
     case SEARCH_TOGGLE:
+      if (state.sidebar === true) {
+        return { ...state, search: !state.search, sidebar: false };
+      }
       return { ...state, search: !state.search };
     case SIDEBAR_TOGGLE:
+      if (state.search === true) {
+        return { ...state, sidebar: !state.sidebar, search: false };
+      }
       return { ...state, sidebar: !state.sidebar };
     default:
       return state;
