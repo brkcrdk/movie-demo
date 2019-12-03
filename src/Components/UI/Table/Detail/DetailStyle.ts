7ba400e1f9d54f8fcfa571ab40bff91d4acdb2c5
Burak <brkcrdk@gmail.com>
2019-12-03 17:29:37 +0200

    Bug fix

--- src/Components/UI/Table/Detail/DetailStyle.ts
+++ src/Components/UI/Table/Detail/DetailStyle.ts
@@ -6,23 +6,23 @@ interface ExpandableProps {
 
 export const Expandable = styled.div`
   width: 100%;
-  max-height: ${(p: ExpandableProps) => (p.expand ? "100%" : "0")};
-  transition: max-height 0.5s;
-  border-bottom: 1px solid lightgray;
-
   #content {
-    width: 100%;
     display: ${(p: ExpandableProps) => (p.expand ? "grid" : "none")};
+    grid-template-columns: repeat(2, 6fr);
+    width: 100%;
+    border-top: 1px solid red;
+    padding: 0.5em;
+    max-height: ${(p: ExpandableProps) => (p.expand ? "100%" : "0")};
+    transition: max-height 0.5s;
   }
 `;
-export const Content = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: repeat(2, 6fr);
-`;
+export const Content = styled.div``;
 
 export const Slayt = styled.div`
   display: grid;
   align-items: center;
   grid-template-columns: repeat(2, 6fr);
+  p {
+    margin: 0 0.5em;
+  }
 `;
