b9eb7de22e6f711315acd5ecd2f5ca708d84f74c
Burak <brkcrdk@gmail.com>
2019-12-05 14:43:43 +0200

    actor fetch action added

--- src/Components/UI/Actors/Actors.tsx
+++ src/Components/UI/Actors/Actors.tsx
@@ -1,22 +1,40 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { Tab, Tabs, Content } from "./ActorStyle";
-import { Cast } from "../../../store/serverTypes";
+import { Cast, ActorBio } from "../../../store/serverTypes";
+import { fetchActor } from "../../../store/Actor/action";
+import { useDispatch, useSelector } from "react-redux";
 import { imgUrl } from "../../../config";
 interface Props {
   credits: {
     cast: Cast[];
   };
 }
+interface ActorStore {
+  actorStore: {
+    actor: ActorBio[];
+  };
+}
 const Actors: React.FC<Props> = ({ credits }) => {
   const [active, setActive] = useState(0);
+
+  const dispatch = useDispatch();
+
   const actors = credits.cast.filter((actor, index) => {
     return index < 5;
   });
+  useEffect(() => {
+    const firstActor = actors[0].id;
+    dispatch(fetchActor(firstActor));
+  }, [dispatch, actors]);
 
-  const handleTab = (index: number, actorId: number) => {
-    if (active !== index) setActive(index);
-  };
-
+  const handleTab = useCallback(
+    (index: number, actorId: number) => {
+      if (active !== index) setActive(index);
+      dispatch(fetchActor(actorId));
+    },
+    [dispatch]
+  );
+  const actorBio = useSelector((state: ActorStore) => state.actorStore.actor);
   return (
     <div>
       <Tabs>
