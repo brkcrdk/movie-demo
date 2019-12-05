7dd4dc10d295c97c80530aa7007a5d1b3f6c67b8
Burak <brkcrdk@gmail.com>
2019-12-05 15:31:04 +0200

    Cleaning

--- src/Components/UI/Actors/Actors.tsx
+++ src/Components/UI/Actors/Actors.tsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import { Tab, Tabs, Content } from "./ActorStyle";
 import { Cast, ActorBio } from "../../../store/serverTypes";
-import { fetchActor } from "../../../store/Actor/action";
-import { useDispatch, useSelector } from "react-redux";
-import { imgUrl } from "../../../config";
+
 interface Props {
   credits: {
     cast: Cast[];
@@ -18,26 +16,13 @@ interface ActorStore {
 const Actors: React.FC<Props> = ({ credits }) => {
   const [active, setActive] = useState(0);
 
-  const dispatch = useDispatch();
-
   const actors = credits.cast.filter((actor, index) => {
     return index < 5;
   });
 
   const handleTab = (index: number, actorId: number) => {
     if (active !== index) setActive(index);
-    dispatch(fetchActor(actorId));
   };
-  const actorBio = useSelector((state: ActorStore) => state.actorStore.actor);
-  const isLoading = useSelector(
-    (state: ActorStore) => state.actorStore.isLoading
-  );
-  const renderBio = actorBio && (
-    <div>
-      <span>{actorBio.biography}</span>
-      <span>{actorBio.popularity}</span>
-    </div>
-  );
 
   return (
     <div>
