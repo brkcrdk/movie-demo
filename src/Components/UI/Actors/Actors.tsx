bb7b498c47e76b46100815e92055b2c9b2fae5b0
Burak <brkcrdk@gmail.com>
2019-12-05 16:50:47 +0200

    Cleaning

--- src/Components/UI/Actors/Actors.tsx
+++ src/Components/UI/Actors/Actors.tsx
@@ -3,7 +3,6 @@ import { Tab, Tabs, Content } from "./ActorStyle";
 import { useDispatch } from "react-redux";
 import { Cast } from "../../../store/serverTypes";
 import { fetchActor } from "../../../store/Actor/action";
-import ActorInfo from "./ActorInfo";
 interface Props {
   credits: {
     cast: Cast[];
@@ -15,19 +14,11 @@ const Actors: React.FC<Props> = ({ credits }) => {
   const actors = credits.cast.filter((actor, index) => {
     return index < 5;
   });
-  const dispatch = useDispatch();
-  const handleTab = useCallback(() => {
-    setActive(actors[0].id);
-  }, []);
-
   return (
     <div>
       {actors.map((actor, id) => (
-        <li key={id} onClick={handleTab}>
-          {actor.name}
-        </li>
+        <li key={id}>{actor.name}</li>
       ))}
-      <ActorInfo />
     </div>
   );
 };
