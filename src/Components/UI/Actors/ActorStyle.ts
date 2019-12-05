d3814c2c393c17b8c49f1aec72940a84a102aef2
Burak <brkcrdk@gmail.com>
2019-12-05 13:23:57 +0200

    Actors tab added

---
+++ src/Components/UI/Actors/ActorStyle.ts
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+interface TabProps {
+  active: boolean;
+}
+export const Tabs = styled.div`
+  overflow: hidden;
+  background: #fff;
+  font-family: Open Sans;
+  height: 3em;
+  width: 100%;
+`;
+
+export const Tab = styled.button`
+  border: none;
+  outline: none;
+  cursor: pointer;
+  position: relative;
+  width: 49%;
+  margin-right: 0.1em;
+  font-size: 1.1em;
+  border: ${(p: TabProps) => (p.active ? "1px solid #ccc" : "")};
+  border-bottom: ${(p: TabProps) => (p.active ? "none" : "")};
+  background-color: ${(p: TabProps) => (p.active ? "white" : "lightgray")};
+  height: ${(p: TabProps) => (p.active ? "3em" : "2.6em; top:.4em")};
+  transition: background-color 0.5s ease-in-out;
+  :hover {
+    background-color: white;
+    outline: none;
+  }
+  > i {
+    padding-left: 1em;
+  }
+`;
+interface ContentProps {
+  active: boolean;
+}
+export const Content = styled.div`
+  ${(p: ContentProps) => (p.active ? "" : "display:none")};
+  width: 100%;
+  height: 100%;
+  transition: transform 300ms ease-in-out;
+  animation: fade-in 2s;
+  @keyframes fade-in {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
--- src/Components/UI/Actors/Actors.tsx
+++ src/Components/UI/Actors/Actors.tsx
@@ -1,10 +1,23 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Tab, Tabs, Content } from "./ActorStyle";
 interface Props {}
 //TODO: Add tabs to here..
 const Actors: React.FC<Props> = () => {
+  const [active, setActive] = useState(0);
   const numbers = [1, 2, 3, 4];
-  return <div></div>;
+  return (
+    <div>
+      <Tabs>
+        {numbers.map((number, key) => (
+          <Tab active={active === key} key={key}>{`Tab ${key}`}</Tab>
+        ))}
+      </Tabs>
+
+      {numbers.map((number, key) => (
+        <Content active={active === key}>Actor {key}</Content>
+      ))}
+    </div>
+  );
 };
 
 export default Actors;
