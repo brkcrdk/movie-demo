caf0e0dd5b8a767f9d5c45670ae4ef82d05fdc29
Burak <brkcrdk@gmail.com>
2019-12-10 14:32:12 +0200

    Movie card design

--- src/Components/Pages/Favourites/Favourites.tsx
+++ src/Components/Pages/Favourites/Favourites.tsx
@@ -12,13 +12,16 @@ interface FavStore {
 
 const Container = styled.div`
   margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 const Wrapper = styled.div`
   margin: 0 5em;
   margin-bottom: 1em;
   display: grid;
-  grid-template-columns: repeat(4, 3fr);
-  grid-gap: 3em;
+  grid-template-columns: repeat(5, 2fr);
+  grid-gap: 2em;
 `;
 const Favourites: React.FC<Props> = () => {
   const favMovies = useSelector(
@@ -26,9 +29,18 @@ const Favourites: React.FC<Props> = () => {
   );
   const renderFavs =
     favMovies &&
-    favMovies.map((movie, index) => (
-      <Card posterPath={movie.movie.poster_path} name={movie.movie.title} />
-    ));
+    favMovies.map((item, index) => {
+      const movie = item.movie;
+      return (
+        <Card
+          key={index}
+          posterPath={movie.poster_path}
+          name={movie.title}
+          voteAvg={movie.vote_average}
+          releaseDate={movie.release_date}
+        />
+      );
+    });
   return (
     <Container>
       <Wrapper>
--- src/Components/UI/Card/Card.tsx
+++ src/Components/UI/Card/Card.tsx
@@ -1,43 +1,51 @@
 import React from "react";
 import styled from "styled-components";
 import { imgUrl } from "../../../config";
-import { colours } from "../../../utils";
+import { colours, fonts } from "../../../utils";
 
 interface Props {
   posterPath: string;
   name: string;
+  voteAvg: number;
+  releaseDate: string;
+  key?: number;
 }
 
 const Container = styled.div`
-  width: 15em;
+  width: 13em;
   box-shadow: ${colours.darkerBoxShadow};
   position: relative;
-
-  &:hover > div {
-    display: grid;
-    position: absolute;
-    top: 0;
-    width: 100%;
-  }
-  > div {
-    display: none;
-  }
+  font-family: ${fonts.aBeeZee};
   > img {
     width: 100%;
   }
-  span {
-    width: 100%;
-    background-color: rgba(0, 0, 1, 0.2);
+`;
+const InfoContainer = styled.div`
+  width: 100%;
+  display: grid;
+  > span {
+    text-align: center;
+    padding: 0.5em 0;
   }
 `;
-
-const Card: React.FC<Props> = ({ posterPath, name }) => {
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0;
+  padding: 0.5em;
+`;
+const Card: React.FC<Props> = ({ posterPath, name, voteAvg, releaseDate }) => {
   return (
     <Container>
       <img src={`${imgUrl}/w500/${posterPath}`} />
-      <div>
+      <InfoContainer>
         <span>{name}</span>
-      </div>
+        <Bottom>
+          <span>{voteAvg}/10</span>
+          <span>{releaseDate}</span>
+        </Bottom>
+      </InfoContainer>
     </Container>
   );
 };
