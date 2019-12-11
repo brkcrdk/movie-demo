45375800abd320c2b7217e5d9de3a8b642e55dc7
Burak <brkcrdk@gmail.com>
2019-12-11 10:47:39 +0200

    Modal page design

--- src/Components/UI/Modal/Modal.tsx
+++ src/Components/UI/Modal/Modal.tsx
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 import { Container, ModalContent, Close } from "./ModalStyle";
+import { useHistory } from "react-router-dom";
 interface Props {}
 
 const Modal: React.FC<Props> = () => {
   const [toggle, setToggle] = useState(true);
+  const history = useHistory();
+
   const handleToggle = () => {
     setToggle(false);
+    history.goBack();
   };
 
   return (
     <Container toggle={toggle}>
       <ModalContent>
         <Close onClick={handleToggle}>&times;</Close>
+
         <p>Modal Content</p>
       </ModalContent>
     </Container>
--- src/Components/UI/Modal/ModalStyle.ts
+++ src/Components/UI/Modal/ModalStyle.ts
@@ -4,25 +4,23 @@ interface ContainerProps {
   toggle: boolean;
 }
 export const Container = styled.div`
-  display: ${(p: ContainerProps) =>
-    p.toggle ? "block" : "none"}; /* Hidden by default */
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
+  display: ${(p: ContainerProps) => (p.toggle ? "block" : "none")};
+  position: fixed;
+  z-index: 5;
   left: 0;
   top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0, 0, 0); /* Fallback color */
-  background-color: rgba(0, 0, 0, 0.4);
+  width: 50%;
+  height: 50%;
+  overflow: auto;
+  background-color: transparent;
 `;
 
 export const ModalContent = styled.div`
   background-color: #fefefe;
-  margin: 15% auto; /* 15% from the top and centered */
+  margin: 15% auto;
   padding: 20px;
   border: 1px solid #888;
-  width: 80%; /* Could be more or less, depending on screen size */
+  width: 80%;
 `;
 
 export const Close = styled.span`
