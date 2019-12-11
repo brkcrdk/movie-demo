62754d64a506c65bcbf12d4276d5fc260b6840a3
Burak <brkcrdk@gmail.com>
2019-12-11 13:03:24 +0200

    Detail page actions

--- src/Components/Pages/MovieDetail/MovieDetail.tsx
+++ src/Components/Pages/MovieDetail/MovieDetail.tsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchDetails } from "../../../store/Detail/action";
+import { IDetail } from "../../../store/serverTypes";
 import Modal from "../../UI/Modal/Modal";
-interface Props {}
 
+interface Props {}
+interface DetailProps {
+  detailStore: {
+    movie: IDetail;
+    isLoading: boolean;
+  };
+}
 const MovieDetail: React.FC<Props> = () => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  useEffect(() => {
+    if (id) {
+      const movieId = parseFloat(id);
+      dispatch(fetchDetails(movieId));
+    }
+  }, [dispatch, id]);
+  const movie = useSelector((state: DetailProps) => state.detailStore.movie);
+  const isLoading = useSelector(
+    (state: DetailProps) => state.detailStore.isLoading
+  );
   return <Modal />;
 };
 
