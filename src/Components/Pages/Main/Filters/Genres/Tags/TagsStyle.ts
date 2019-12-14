c57f54cbcc5899ddc5b3f1f93cf3e4881daf7c73
brkcrdk <brkcrdk@gmail.com>
2019-12-14 16:19:27 +0200

    Cleaning

--- src/Components/Pages/Main/Filters/Genres/Tags/TagsStyle.ts
+++ src/Components/Pages/Main/Filters/Genres/Tags/TagsStyle.ts
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 import { fonts } from "../../../../../../utils";
+
 export const Container = styled.div`
   width: 100%;
   display: flex;
   justify-content: flex-start;
   align-items: center;
   padding-left: 2em;
-  padding-bottom: 0.5em;
   overflow: hidden;
   flex-wrap: wrap;
   z-index: -1;
