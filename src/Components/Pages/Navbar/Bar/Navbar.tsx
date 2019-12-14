4218b50728b9f79e07c9dfb72de66068f064fd99
brkcrdk <brkcrdk@gmail.com>
2019-12-14 19:39:36 +0200

    Toggle action added for modal

--- src/Components/Pages/Navbar/Bar/Navbar.tsx
+++ src/Components/Pages/Navbar/Bar/Navbar.tsx
@@ -3,25 +3,41 @@ import Search from "../../Search/Search";
 import Pages from "../Pages";
 import { Container, Desktop, Mobile, Icons, Section } from "./NavbarStyle";
 import { useDispatch, useSelector } from "react-redux";
-import { toggleSidebar, toggleSearch } from "../../../../store/Toggles/action";
+import { useHistory } from "react-router-dom";
+import {
+  toggleSidebar,
+  toggleSearch,
+  toggleModal
+} from "../../../../store/Toggles/action";
 import { Bookmark, ToggleIcons } from "../../../UI/Icons/Icons";
 interface Props {}
 interface ToggleProps {
   toggle: {
     mobileSection: string;
+    modal: boolean;
   };
 }
 const Navbar: React.FC<Props> = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
   const section = useSelector(
     (state: ToggleProps) => state.toggle.mobileSection
   );
+  const modalToggle = useSelector((state: ToggleProps) => state.toggle.modal);
   const handleSidebar = useCallback(() => {
     dispatch(toggleSidebar());
-  }, [dispatch]);
+    if (modalToggle) {
+      dispatch(toggleModal());
+      history.goBack();
+    }
+  }, [dispatch, modalToggle]);
   const handleSearch = useCallback(() => {
     dispatch(toggleSearch());
-  }, [dispatch]);
+    if (modalToggle) {
+      dispatch(toggleModal());
+      history.goBack();
+    }
+  }, [dispatch, modalToggle]);
   return (
     <Container>
       <Desktop>
