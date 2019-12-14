9626ffb086daf16e18e95db9d7bb1ca6cfcec83b
brkcrdk <brkcrdk@gmail.com>
2019-12-14 22:51:00 +0200

    Cleaning

--- src/Components/Pages/Navbar/Sidebar.tsx
+++ src/Components/Pages/Navbar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
+import { device } from "../../../utils";
 import Pages from "../Navbar/Pages";
 interface Props {}
 interface ToggleStore {
@@ -12,12 +13,18 @@ interface ContainerProps {
   toggle: boolean;
 }
 const Container = styled.div`
-  height: 100%;
-  width: ${(p: ContainerProps) => (p.toggle ? "10em" : "0")};
-  overflow-x: hidden;
-  position: fixed;
-  z-index: 1;
-  transition: 0.5s;
+  @media ${device.mobileS} {
+    display: none;
+  }
+  @media ${device.tablet} {
+    display: block;
+    height: 100%;
+    width: ${(p: ContainerProps) => (p.toggle ? "10em" : "0")};
+    overflow-x: hidden;
+    position: fixed;
+    z-index: 1;
+    transition: 0.5s;
+  }
 `;
 const Sidebar: React.FC<Props> = () => {
   const toggle = useSelector((state: ToggleStore) => state.toggle.sidebar);
